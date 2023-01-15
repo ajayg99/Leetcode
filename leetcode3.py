ab3bb2716e2f04eee3be94df57b063ecd7059c62
Ajay Gijandren <g.ajaygg@outlook.com>
2023-01-15 11:13:10 -0500

    leetcode3

--- leetcode3.py
+++ leetcode3.py
@@ -1,15 +1,15 @@
 s = 'abcabcbb'
 
-l = 0
-r = 0
-result = 0
-seen = set()
+l = 0  #left pointer
+r = 0  #right pointer
+result = 0 #result length
+seen = set()  #set to hold seen values
 
-for r in range(0,len(s)):
-    while s[r] in seen:
+for r in range(0,len(s)): # increment right pointer
+    while s[r] in seen: # if right pointer in set, delete the value of right pointer in set and increment left pointer
         seen.remove(s[l])
         l+=1
-    result = max(result,r-l+1)
+    result = max(result,r-l+1) # else find the length 
     seen.add(s[r])
 
 print(result)
