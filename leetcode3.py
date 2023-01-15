93f32e76d48dd0a13852bc1262a463f2320d8a9e
Ajay Gijandren <g.ajaygg@outlook.com>
2023-01-15 12:48:37 -0500

    leetcode3

--- leetcode3.py
+++ leetcode3.py
@@ -13,3 +13,20 @@ for r in range(0,len(s)): # increment right pointer
     seen.add(s[r])
 
 print(result)
+
+
+#Approach 2
+
+s = 'abcabcbb'
+temp = set()
+res = 0
+for i in range(0,len(s)):  #loop for all the element
+    for j in range(i,len(s)): #loop for each element starting for its position
+        if s[j] in temp: #if s[j] is in temp (duplicate)
+            res = max(res,len(temp)) # the temp contains all the non duplicate elements, hence take the len 
+            temp = set() # re initailize temp
+            break # break the inner loop -> i+=1
+        else:
+            temp.add(s[j]) #else add the value to the set
+res = max(res,len(temp)) #if the string contains no duplicate 
+print(res)
